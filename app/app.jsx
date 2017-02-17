d3a9665b452d37a7b839d9f60251e89145cc1ab7
Stuart Tiedemann <tiedemannstuart@gmail.com>
2017-02-17 14:33:58 -0600

    Configured webpack and created TodoApp.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
 
 // Load foundation
 $(document).foundation();
@@ -10,6 +11,6 @@ require('style!css!sass!applicationStyles');
 
 
 ReactDOM.render(
-  <p>React Todos.</p>,
+  <TodoApp/>,
   document.getElementById('app')
 );
