eabf1eedbe194f8142875727f892068c37c7e82f
Stuart Tiedemann <tiedemannstuart@gmail.com>
2017-02-17 21:47:31 -0600

    play around with spy karma test

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+  onFormSubmit: function(e) {
+    e.preventDefault();
+    var text = this.refs.text.value;
+
+    if (text.length > 0) {
+      // Clear the input field
+      this.refs.text.value = '';
+      // Make the text available to the parent TodoApp.jsx container
+      this.props.onAddTodo(text);
+    } else {
+      this.refs.text.focus();
+    }
+  },
+  render: function () {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <div>
+          <input type="text" ref="text" placeholder="Enter a new todo" />
+        </div>
+        <div>
+          <button className="button expanded">Add Todo</button>
+        </div>
+      </form>
+    );
+  }
+});
+
+module.exports = AddTodo;
