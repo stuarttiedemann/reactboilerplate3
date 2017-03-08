3e49eff90797e52819d88893cf8c63458bb223ea
Stuart Tiedemann <tiedemannstuart@gmail.com>
2017-03-07 21:50:59 -0500

    refactor redux to work with localStorage

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -4,56 +4,20 @@ import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
 
 var moment = require('moment');
-
-// node package to generate unique time stamped id's
-// npm install node-uuid --save-dev
 var uuid = require('node-uuid');
-var TodoAPI = require('TodoAPI');
-
 var TodoApp = React.createClass({
-  getInitialState: function () {
-    return {
-      showCompleted: false,
-      searchText: '',
-      todos: TodoAPI.getTodos()
-    };
-  },
-  componentDidUpdate: function () {
-    TodoAPI.setTodos(this.state.todos);
-  },
   
-  handleAddToDo: function (text) {
-    this.setState({
-      todos: [
-        ...this.state.todos,
-        {
-          id: uuid(),
-          text: text,
-          completed: false,
-          createdAt: moment().unix(),
-          completedAt: undefined
-        }
-      ]
-    });
-  },
-  handleSearch: function (showCompleted, searchText) {
-    this.setState({
-      showCompleted: showCompleted,
-      searchText: searchText.toLowerCase()
-    });
-  },
   render: function () {
-    var {todos, showCompleted, searchText} = this.state;
-    var filteredTodos = TodoAPI.filterTodos(todos,showCompleted,searchText);
+    
     return (
       <div>
         <h1 className="page-title">My Awesome Todo App</h1>
         <div className="row">
           <div className="column small-centered small-11 medium-6 large-5">
             <div className="container">
-              <TodoSearch onSearch={this.handleSearch} />
+              <TodoSearch />
               <TodoList />
-              <AddTodo onAddTodo={this.handleAddToDo} />
+              <AddTodo />
             </div>
           </div>
         </div>
--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -22,6 +22,25 @@ describe('Actions', () => {
     expect(res).toEqual(action);
   });
 
+  it('should generate add todos action object', () => {
+    var todos = [{
+      id: '111',
+      text: 'anything',
+      completed: false,
+      completedAt: undefined,
+      createdAt: 33000
+    }];
+
+    var action =  {
+      type: 'ADD_TODOS',
+      todos 
+    };
+
+    var res = actions.addTodos(todos);
+
+    expect(res).toEqual(action);
+  });
+
   it('should toggle show completed', () => {
     var action = {
       type: 'TOGGLE_SHOW_COMPLETED'
